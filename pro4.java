3a3e8e8c0938b8d788833e0583e28e7e4920bc83
RajaPrajapati001 <rajaprajapati3454@gmail.com>
2024-08-29 15:48:54 +0530

    hello

--- pro4.java
+++ pro4.java
@@ -3,6 +3,7 @@ public class pro4 {
     static Scanner sc=new Scanner(System.in);
     public static void main(String []args){
         System.out.print("Enter any number:");
+        System.out.print("Enter any number:");
         
     }
 }
